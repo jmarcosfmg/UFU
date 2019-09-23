31beb87225c0951dc89a371f4a1c6a9b8df725c7
jmarcosfmg <43149260+jmarcosfmg@users.noreply.github.com>
2019-09-23 01:08:29 -0300

    Add files via upload

--- trabppi/javascript/script.js
+++ trabppi/javascript/script.js
@@ -1,3 +1,4 @@
-import "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+@import url("https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
